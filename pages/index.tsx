39fbb1e782f75f8ed160b5a49be38866635e9c6b
kushMayank <mayank.kushwaha07@gmail.com>
2023-08-10 19:23:53 +0530

    just keeping mobile first in mind

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,15 +9,13 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-
-
-      <div className="hover:shadow-indigo-500/40 text-center text-8xl relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+     <div className="hover:shadow-indigo-500/40 text-center text-5xl sm:text-8xl relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
       >
         <div className='font-mono'>MAYANK KUSHWAHA</div>
         <div className='font-mono py-10'>X</div>
         <div className='font-mono'>WEB DEVELOPER</div>
         <Link href="/about-me">
-          <h2 className={` animate-bounce p-10 text-2xl font-semibold`}>
+          <h2 className={` animate-bounce p-20 sm:p30 text-2xl font-semibold`}>
             About Me{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
